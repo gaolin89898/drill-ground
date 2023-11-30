e39059c897e09fbaab4da4ad45e19504ecf53c16
gl <3289149570@qq.com>
2023-11-30 17:51:55 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,62 @@
+import { defineConfig } from "vite";
+import Vue from '@vitejs/plugin-vue'
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ArcoResolver } from "unplugin-vue-components/resolvers";
+import VueMacros from 'unplugin-vue-macros/vite';
+import path from 'path';
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+        imports: [
+          'vue',
+          'vue/macros',
+          'vue-router',
+          {
+            axios: [['default', 'axios']],
+          },
+        ],
+        dts: './auto-imports.d.ts',   
+    }),
+    Components({
+      dts: true,
+        resolvers: [ArcoResolver()],
+        types: [
+          {
+            from: 'vue-router',
+            names: ['RouterLink', 'RouterView'],
+          },
+        ],
+        include: [/\.vue$/, /\.vue\?vue/],
+        deep: true,
+        allowOverrides: false,
+        dirs: [
+          'src/components',
+          'src/layouts',
+          'src/views/LowbiBigScreen/componets',
+        ],
+    }),
+    VueMacros({
+      plugins: {
+        vue: Vue()
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  // css: {
+  //   /* CSS 预处理器 */
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: '@import "./src/assets/styles/index.scss"',
+  //     },
+  //   },
+  // },
+});
