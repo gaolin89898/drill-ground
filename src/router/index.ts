d6a8a39891c9bf08e2f77c5e7a0da4488bfedf01
gl <3225532868@qq.com>
2025-06-30 17:40:33 +0800

    提交

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,69 +24,21 @@ const routes: RouteRecordRaw[] = [
         },
       },
       {
-        path: '/blog_management',
-        name: 'blog',
-        meta: {
-          title: '博客管理',
-          icon: 'drill-boke',
-          hidden: false,
-        },
-        redirect: '/blog_management/article_management',
-        children: [
-          {
-            path: '/blog_management/article_management',
-            name: 'article',
-            component: () =>
-              import('@/view/blog_management/article_management/index.vue'),
-            meta: {
-              title: '文章管理',
-              icon: 'drill-16',
-              hidden: false,
-            },
-          },
-          {
-            path: '/blog_management/addArticle',
-            name: 'addArticle',
-            component: () =>
-              import(
-                '@/view/blog_management/article_management/addArticle.vue'
-              ),
-            meta: {
-              title: '创建文章',
-              icon: 'drill-16',
-              hidden: false,
-            },
-          },
-          {
-            path: '/blog_management/tagClassify_management',
-            name: 'tagClassify',
-            component: () =>
-              import('@/view/blog_management/tagClassify_management/index.vue'),
-            meta: {
-              title: '标签，分类管理',
-              icon: 'drill-biaoqian',
-              hidden: false,
-            },
-          },
-          {
-            path: '/blog_management/recycleBin_management',
-            name: 'recycleBin',
-            component: () =>
-              import('@/view/blog_management/recycleBin_management/index.vue'),
-            meta: {
-              title: '回收站',
-              icon: 'drill-huishouzhan',
-              hidden: false,
-            },
-          },
-        ],
-      },
-      {
         path: '/fileExplorer',
         name: 'fileExplorer',
         component: () => import('@/view/fileExplorer/index.vue'),
         meta: {
           title: '资源管理器',
+          icon: 'drill-ziyuanguanliqi',
+          hidden: false,
+        },
+      },
+      {
+        path: '/book_manage',
+        name: 'book_manage',
+        component: () => import('@/view/book_manage/index.vue'),
+        meta: {
+          title: '阅读图书管理',
           icon: 'drill-ziyuanguanliqi',
           hidden: false,
         },
